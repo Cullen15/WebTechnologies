aa395e4d6add01acee3b2464f8f70ffce9cb7d62
cullen.bertsch <cullen.bertsch@umconnect.umt.edu>
2026-02-02 16:18:39 -0700

    updated

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// Favorite Websites
+console.log("Favorite Websites:");
+console.log("1. YouTube – I use it for for learning and entertainment");
+console.log("2. Vimeo – Good for inspiration for videography and filmmaking");
+console.log("3. Reddit – Great for solving problems");
+
+// Favorite Games
+
+console.log("Favorite Games:");
+console.log("1. We Were Here Forever – focuses on teamwork and communication");
+console.log("2. Rainbow 6 Seige – Strategy based shooter");
+console.log("3. Rocket League – fast-paced and competitive");
+
+// Favorite Artists & Cultural Importance
+console.log("Favorite Artists and Their Cultural Importance:");
+console.log("1. Travis Rice – inspired me and helped shape modern snowboarding culture");
+console.log("2. Red Bull Media House – changed action sports storytelling");
+console.log("3. Casey Neistat – influenced digital filmmaking and YouTube culture");
+
+// Question and Answer Interaction
+let userAnswer = prompt("What is your favorite type of game?");
+
+console.log("What is your favorite hobby:");
+console.log(userAnswer);
